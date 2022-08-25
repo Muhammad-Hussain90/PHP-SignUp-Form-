8a8075a0e4792cf09464a2d18446a5b93f90c780
muhammadhussain <zahakhussain90@gmail.com>
2022-08-25 22:14:52 +0500

    jquery commented

--- sign-up.php
+++ sign-up.php
@@ -114,19 +114,19 @@ if($exists) {
 <!-- Optional JavaScript --> 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
-<script src="
+<!-- <script src="
 https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="
 sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
     crossorigin="anonymous">
-</script>
+</script> -->
     
-<script src="
+<!-- <script src="
 https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
     integrity=
 "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" 
     crossorigin="anonymous">
-</script>
+</script> -->
     
 </body>
 </html>
